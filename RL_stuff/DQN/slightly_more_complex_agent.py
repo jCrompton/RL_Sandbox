5eed197813db651af9aa59ab0d0ce8ffe38148f3
James Crompton <james.crompton@cyclicarx.com>
2018-09-24 20:28:16 -0400

    updates

--- RL_stuff/DQN/slightly_more_complex_agent.py
+++ RL_stuff/DQN/slightly_more_complex_agent.py
@@ -1,3 +1,5 @@
+import argparse
+
 import tensorflow as tf
 import numpy as np
 
@@ -12,23 +14,34 @@ import warnings
 warnings.filterwarnings('ignore')
 
 class Agent:
-    def __init__(self, env, total_episodes=10000, max_steps=100, state_shape=[4,4], batch_size=64, explore_start=1.0, explore_stop=0.01, decay_rate=0.0001, action_size=2, debug=False, learning_rate = 0.0002, memory_size = 1000000, gamma=0.95):
+    def __init__(self, env_name='CartPole-v0', total_episodes=10000, batch_size=64, explore_start=1.0,
+                 explore_stop=0.01, decay_rate=0.0001, debug=False, pretrain_length=64, 
+                 learning_rate=0.0002, memory_size=1000000, gamma=0.95, stack_size=4, debug_rate=6,
+                 dense_architecture=[120, 40], dropout=True, activation=tf.nn.relu, dropout_rate=0.25, 
+                 tensorboard_dir="scratch/dqn", frozen_model_dir="./models/"):
+        env = gym.make(env_name)
+
         self.env = env
-        self.network = DQNetwork(state_shape, action_size, learning_rate)
+        self.stack_size = stack_size
+        self.network = DQNetwork((*env.observation_space.shape, stack_size), env.action_space.n, learning_rate)
         self.saver = tf.train.Saver()
         self.sess = tf.Session()
         self.total_episodes = total_episodes
-        self.memory = Memory(env, memory_size, batch_size)
-        self.max_steps = max_steps
+        self.memory = Memory(env, memory_size, pretrain_length, stack_size)
         self.batch_size = batch_size
-        self.debug=debug
+        self.debug = debug
         self.explore_start = explore_start
         self.explore_stop = explore_stop
         self.decay_rate = decay_rate
         self.decay_step = 0
         self.gamma = gamma 
+        self.debug_rate = debug_rate
+
          # Setup tensorboard 
-        self.writer = tf.summary.FileWriter("scratch/dqn")
+        self.writer = tf.summary.FileWriter(tensorboard_dir)
+
+        # Frozen model dir
+        self.frozen_model_dir = "{}{}.ckpt".format(frozen_model_dir, '{}_{}'.format(env_name, total_episodes))
 
         # Loss
         tf.summary.scalar("DQN_Loss", self.network.loss)
@@ -41,7 +54,7 @@ class Agent:
 
         if (explore_probability > exp_exp_tradeoff):
             # Take random action
-            return one_hot_encode_state(self.env.action_space.sample()), explore_probability
+            return one_hot_encode_action(self.env.action_space.sample(), self.env.action_space.n), explore_probability
 
         else:
             # Predict action from DQN
@@ -49,7 +62,7 @@ class Agent:
             Q_values = self.sess.run(self.network.output, feed_dict={self.network.inputs_:state.reshape((1, *state.shape))})
 
             # Take the biggest Q value
-            return one_hot_encode_state(np.argmax(Q_values)), explore_probability
+            return one_hot_encode_action(np.argmax(Q_values), self.env.action_space.n), explore_probability
 
     def train(self):
         self.sess.run(tf.global_variables_initializer())
@@ -58,28 +71,33 @@ class Agent:
 
         for episode in range(self.total_episodes):
             step = 0 
+            show_episode = False
             done = False
             episode_rewards = []
             state = self.env.reset()
-            state, stacked_frames = stack_frames(deque(), state, True)
+            state, stacked_frames = stack_frames(deque(), state, True, stack_size=self.stack_size)
 
-            if episode % 5 == 0:
-                save_path = self.saver.save(self.sess, "./models/model.ckpt")
+            if episode % (self.total_episodes / self.debug_rate ) <= 1:
+                save_path = self.saver.save(self.sess, self.frozen_model_dir)
                 if self.debug:
-                    print("Model Saved...\n")
+                    show_episode = True
+                    print("Model Saved...\nShowing next episode ({}/{})...\n".format(episode, self.total_episodes))
+
 
             while not done:
+                # If debug enabled and at a far enough checkpoint show episode
+                if self.debug and show_episode:
+                    self.env.render()
+
                 decay_step += 1
 
                 action, explore_probability = self.predict_action(state)
-                next_state, reward, done, _ = self.env.step(one_hot_decode_state(action))
+                next_state, reward, done, _ = self.env.step(one_hot_decode_action(action))
 
                 episode_rewards.append(reward)
 
                 if done:
-                    next_state, stacked_frames = stack_frames(stacked_frames, next_state, False)
-                    # Stop the episode by setting step to max_steps
-                    step = self.max_steps
+                    next_state, stacked_frames = stack_frames(stacked_frames, next_state, False, stack_size=self.stack_size)
                     # Calc total reward of episode
                     total_reward = np.sum(episode_rewards)
 
@@ -89,8 +107,7 @@ class Agent:
                               # 'Training loss: {:.4f}'.format(loss),
                               'Explore P: {:.4f}'.format(explore_probability))
                 else:
-                    next_state, stacked_frames = stack_frames(stacked_frames, next_state, False)
-
+                    next_state, stacked_frames = stack_frames(stacked_frames, next_state, False, stack_size=self.stack_size)
                 self.memory.add((state, action, reward, next_state, done))
 
                 # State + 1 is now the current state
@@ -106,7 +123,7 @@ class Agent:
             done_minibatch = np.array([sample[4] for sample in batch])
             target_Qs_batch = []
 
-            # Get Qs for next_state
+            # Get Qs for next_states
             Qs_next_state = self.sess.run(self.network.output, feed_dict={self.network.inputs_:next_states_minibatch})
 
             # Set Qtarget to reward if episode ends at s+1 otherwise Qtarget = reward * gamma*maxQ(s',a')
@@ -135,11 +152,12 @@ class Agent:
             self.writer.flush()
 
     def play(self, num_episodes=1):
+        self.saver.restore(self.sess, self.frozen_model_dir)
         for i in range(num_episodes):
             total_score = 0
             done = False
             state = self.env.reset()
-            state, stacked_frames = stack_frames(deque(), state, True)
+            state, stacked_frames = stack_frames(deque(), state, True, stack_size=self.stack_size)
             while not done:
                 # Get predicted q-values from trained network (if you trained it)
                 Q_values = self.sess.run(self.network.output, feed_dict={self.network.inputs_:state.reshape((1, *state.shape))})
@@ -151,16 +169,17 @@ class Agent:
                 # Add to current total_rewards
                 total_score += reward
                 # Convert back into stacked form for next prediction 
-                state, stacked_frames = stack_frames(stacked_frames, state, True)
-                if self.debug:
-                    self.env.render()
+                state, stacked_frames = stack_frames(stacked_frames, state, True, stack_size=self.stack_size)
+
+                self.env.render()
+
             if self.debug:
                 print("Finished episode {} with a total score of {}".format(i, total_score))
 
 
-def stack_frames(stacked_frames, state, is_new_episode, stack_size=4, max_len=4):
+def stack_frames(stacked_frames, state, is_new_episode, stack_size=4):
     if is_new_episode:
-        stacked_frames = deque([np.zeros(state.shape, dtype=np.int) for i in range(stack_size)], maxlen=max_len)
+        stacked_frames = deque([np.zeros(state.shape, dtype=np.int) for i in range(stack_size)], maxlen=stack_size)
 
         # Since we're in a new episode coppy the same frame stack_size times
         for _ in range(stack_size):
@@ -168,16 +187,17 @@ def stack_frames(stacked_frames, state, is_new_episode, stack_size=4, max_len=4)
     else:
         stacked_frames.append(state)
 
-    return np.stack(stacked_frames), stacked_frames
+    return np.stack(stacked_frames, axis=len(state.shape)), stacked_frames
 
-def one_hot_encode_state(state, action_space=2):
-    return [1,0] if state == 0 else [0,1]
+def one_hot_encode_action(action, action_space):
+    return np.identity(action_space)[action]
 
-def one_hot_decode_state(state):
-    return np.argmax(state)
+def one_hot_decode_action(action):
+    return np.argmax(action)
 
 class DQNetwork:
-    def __init__(self, state_size, action_size, learning_rate, name='DQNetwork', hidden_layer_size = 120):
+    def __init__(self, state_size, action_size, learning_rate, name='DQNetwork', dense_architecture=[120, 40],
+                 dropout=True, activation=tf.nn.relu, dropout_rate=0.25):
         self.state_size = state_size
         self.action_size = action_size
         self.learning_rate = learning_rate
@@ -186,13 +206,19 @@ class DQNetwork:
             # Create placeholders
             # Where *state_size just unfolds the state_size array e.g.: if state_size was [5,5] then we'd write [None, 5, 5]
             self.inputs_ = tf.placeholder(tf.float32, [None, *state_size], name='inputs')
-            self.actions_ = tf.placeholder(tf.float32, [None,2], name='actions')
+            self.actions_ = tf.placeholder(tf.float32, [None,action_size], name='actions')
 
             self.target_Q = tf.placeholder(tf.float32, [None], name='target')
 
-            self.flatten = tf.layers.flatten(self.inputs_)
-            self.fc = tf.layers.dense(inputs=self.flatten, units=hidden_layer_size, activation=tf.nn.elu, name='fc1')
-            self.output = tf.layers.dense(inputs=self.fc, units=action_size)
+            # Initialize with Flattening layer (flatten stack, probably not best thing to do but w.e)
+            fc_i_layer = tf.layers.flatten(self.inputs_)
+
+            for i, layer_units in enumerate(dense_architecture):
+                fc_i_layer = tf.layers.dense(inputs=fc_i_layer, units=layer_units, activation = activation, name='full_connected_{}'.format(i))
+                if dropout:
+                    fc_i_layer = tf.layers.dropout(inputs=fc_i_layer, rate=dropout_rate)
+
+            self.output = tf.layers.dense(inputs=fc_i_layer, units=action_size)
 
             # Q is predicted Q
             self.Q = tf.reduce_sum(tf.multiply(self.output, self.actions_), axis=1)
@@ -200,12 +226,14 @@ class DQNetwork:
             # The loss is the square diff between predicted Q and target Q
             self.loss = tf.reduce_mean(tf.square(self.Q - self.target_Q))
 
-            self.optimizer = tf.train.RMSPropOptimizer(self.learning_rate).minimize(self.loss)
+            self.optimizer = tf.train.AdamOptimizer(self.learning_rate).minimize(self.loss)
 
 
 class Memory():
-    def __init__(self, env, max_size, pretrain_length):
+    def __init__(self, env, max_size, pretrain_length, stack_size):
         self.buffer = deque(maxlen=max_size)
+        self.stack_size = stack_size
+
         self._prepopulate(env, pretrain_length)
 
     def add(self, experience):
@@ -220,13 +248,13 @@ class Memory():
         for i in range(pretrain_length):
             if i == 0:
                 state = env.reset()
-                state, stacked_frames = stack_frames(None, state, True)
+                state, stacked_frames = stack_frames(None, state, True, stack_size=self.stack_size)
 
             # Random action
-            action = one_hot_encode_state(env.action_space.sample())
+            action = one_hot_encode_action(env.action_space.sample(), env.action_space.n)
 
-            next_state, reward, done, _  = env.step(one_hot_decode_state(action))
-            next_state, stacked_frames = stack_frames(stacked_frames, next_state, False)
+            next_state, reward, done, _  = env.step(one_hot_decode_action(action))
+            next_state, stacked_frames = stack_frames(stacked_frames, next_state, False, stack_size=self.stack_size)
 
             if done:
 
@@ -235,7 +263,7 @@ class Memory():
                 # Reset
                 state = env.reset()
 
-                state, stacked_frames = stack_frames(stacked_frames, state, True)
+                state, stacked_frames = stack_frames(stacked_frames, state, True, stack_size=self.stack_size)
 
             else:
                 self.add((state, action, reward, next_state, done))
@@ -243,9 +271,40 @@ class Memory():
 
 
 
-def __main__(stack_size=4, env_name='CartPole-v1', learning_rate=0.0002, gamma=0.95, batch_size=64, total_episodes=500, memory_size=1000000):
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Slightly more complex deep q-learning algorithm implementation")
+
+    parser.add_argument('--env_name', type=str, default='CartPole-v0')
+    parser.add_argument('--learning_rate', type=float, default=0.0002)
+    parser.add_argument('--batch_size', type=int, default=64)
+    parser.add_argument('--gamma', type=float, default=0.95)
+    parser.add_argument('--total_episodes', type=int, default=2000)
+    parser.add_argument('--explore_start', type=float, default=1.0)
+    parser.add_argument('--explore_stop', type=float, default=0.01)
+    parser.add_argument('--decay_rate', type=float, default=0.0001)
+    parser.add_argument('--memory_size', type=int, default=1000000)
+    parser.add_argument('--pretrain_length', type=int, default=2000)
+    parser.add_argument('--stack_size', type=int, default=4)
+    parser.add_argument('--num_episodes', type=int, default=1)
+
+    parser.add_argument('--dropout', action='store_true')
+    parser.add_argument('--dropout_rate', type=float, default=0.25)
+
+    parser.add_argument('--debug', action='store_true')
+    parser.add_argument('--play', action='store_true')
+
+    args = vars(parser.parse_args())
+
+    play = args.pop('play')
+    num_episodes = args.pop('num_episodes')
+
+    agent = Agent(**args)
+
+    if play:
+        agent.play(num_episodes)
+    else:
+        agent.train()
+        
     
-    pretrain_length = batch_size
 
-
-
+    
